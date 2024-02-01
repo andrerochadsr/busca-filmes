15c4b16218b21dab615b1f11792286ef42f28e8f
Andre Rocha <andre.felipedsr@gmail.com>
2024-02-01 14:09:19 -0300

    att

--- teste.js
+++ teste.js
@@ -1,14 +1,32 @@
 function teste() {
-    fetch('http://www.omdbapi.com/?apikey=42664b20&s=batman+the+dark+knight')
+    const barraPesquisa = document.getElementById('barra-pesquisa').value;
+    const formatado = barraPesquisa.replace(/ /g, '+');
+    fetch(`http://www.omdbapi.com/?apikey=42664b20&s=${formatado}`)
     .then(result => result.json())
     .then(json => {
-        const listaFilmes = json['Search']
-        for (inicio = 0; inicio < listaFilmes.length; inicio++) {
-            const novoElemento = document.createElement('p');
-            novoElemento.setAttribute('id', `nome-${inicio}`)
+        if (json['Response'] == 'True') {
+            const listaFilmes = json['Search'];
+            console.log(listaFilmes)
+            document.getElementById('div').innerHTML = '';
             const div = document.getElementById('div');
-            div.appendChild(novoElemento);
-            document.getElementById(`nome-${inicio}`).innerHTML ='Nome: '+ listaFilmes[inicio]['Title'];
+            //laço para criar novos parágrafos
+            for (inicio = 0; inicio < listaFilmes.length; inicio++) {
+                //cria um parágrafo para os nomes
+                const novoElementoNome = document.createElement('p');
+                //atribui um ID de acordo com a variavel INICIO
+                novoElementoNome.setAttribute('id', `nome-${inicio}`)
+                div.appendChild(novoElementoNome);
+                document.getElementById(`nome-${inicio}`).innerHTML ='Nome: '+ listaFilmes[inicio]['Title'];
+
+                //cria um parágrafo para os anos
+                const novoElementoAno = document.createElement('p')
+                //atribui im ID de acordo com a variavel INICIO
+                novoElementoAno.setAttribute('id', `ano-${inicio}`)
+                div.appendChild(novoElementoAno);
+                document.getElementById(`ano-${inicio}`).innerHTML ='Ano: '+ listaFilmes[inicio]['Year'];
+            }
+        } else {
+            document.getElementById('div').innerHTML = 'ERRO! Tente novamente.';
         }
     })
 }

8fae2640a1220d0d90715290c2ed33f15f086c84
Andre Rocha <andre.felipedsr@gmail.com>
2024-02-03 13:57:23 -0300

    att

--- functions.js
+++ functions.js
@@ -9,6 +9,12 @@ async function func() {
         const div = document.getElementById('div');
         //laço para criar novos parágrafos
         for (inicio = 0; inicio < listaFilmes.length; inicio++) {
+            //cria um container para inserir os filmes e dados.
+            const novoElementoContainer = document.createElement('div');
+            novoElementoContainer.setAttribute('class', 'container-filmes');
+            div.appendChild(novoElementoContainer);
+
+
             //cria uma div para o poster
             const novoElementoIMG = document.createElement('img')
             //atribui a fonte da imagem
@@ -18,26 +24,33 @@ async function func() {
             //atribui as dimensoes 
             novoElementoIMG.setAttribute('width', 153);
             novoElementoIMG.setAttribute('height', 232.8);
-            div.appendChild(novoElementoIMG);
+            //insere o elemento no container dos filmes
+            novoElementoContainer.appendChild(novoElementoIMG);
+
 
             //cria um parágrafo para os nomes
             const novoElementoNome = document.createElement('p');
             //atribui um ID de acordo com a variavel INICIO
             novoElementoNome.setAttribute('id', `nome-${inicio}`);
-            div.appendChild(novoElementoNome);
+            //insere o elemento no container dos filmes
+            novoElementoContainer.appendChild(novoElementoNome);
             document.getElementById(`nome-${inicio}`).innerHTML ='Nome: '+ listaFilmes[inicio]['Title'];
+
 
             //cria um parágrafo para o ano
             const novoElementoAno = document.createElement('p');
             //atribui im ID de acordo com a variavel INICIO
             novoElementoAno.setAttribute('id', `ano-${inicio}`);
-            div.appendChild(novoElementoAno);
+            //insere o elemento no container dos filmes
+            novoElementoContainer.appendChild(novoElementoAno);
             document.getElementById(`ano-${inicio}`).innerHTML ='Ano: '+ listaFilmes[inicio]['Year'];
+
 
             //cria um parágrafo para os tipos (serie ou filme)
             const novoElementoTipo = document.createElement('p');
             novoElementoTipo.setAttribute('id', `tipo-${inicio}`);
-            div.appendChild(novoElementoTipo);
+            //insere o elemento no container dos filmes
+            novoElementoContainer.appendChild(novoElementoTipo);
             document.getElementById(`tipo-${inicio}`).innerHTML ='Tipo: '+listaFilmes[inicio]['Type'];
         }
     } else {
